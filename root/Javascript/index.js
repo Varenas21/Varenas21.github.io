93b44cf2f5446d736c7521fc7926dcda8a832d37
Valeria Arenas <vava.tx66@gmail.com>
2024-05-22 10:58:00 -0600

    Page transitions

--- root/Javascript/index.js
+++ root/Javascript/index.js
@@ -1,7 +1,23 @@
 // Navigate through pages
-function navigateToPage(pageUrl)
+function navigateToPage(url)
 {
-    window.location.href = pageUrl;
+    // Page transition
+    var currentContent = document.querySelector('.content:not(.hidden)');
+    currentContent.classList.add('fade-out');
+
+    // Fade in to new page 
+    setTimeout(function() {
+        currentContent.classList.add('hidden');
+        currentContent.classList.remove('fade-out');
+
+        var newContent = document.getElementById(url);
+        newContent.classList.remove('hidden');
+        newContent.classList.add('fade-in');
+    }, 500);
+
+    setTimeout(function () {
+        window.location.href = url;
+    })
 }
 
 function toggleMenu()
@@ -9,3 +25,4 @@ function toggleMenu()
     var menu = document.querySelector('.menu-items');
     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
+
