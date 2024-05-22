e2b3280670f024373aae9c8afbb0733b88fa55ad
Valeria Arenas <vava.tx66@gmail.com>
2024-05-22 06:03:41 -0600

    Added page navigator

--- root/Javascript/index.js
+++ root/Javascript/index.js
@@ -3,3 +3,9 @@ function navigateToPage(pageUrl)
 {
     window.location.href = pageUrl;
 }
+
+function toggleMenu()
+{
+    var menu = document.querySelector('.menu-items');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
