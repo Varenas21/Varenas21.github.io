2e0b3ccdd2c266b78d73c7ed43e093aa36a6978a
Valeria Arenas <110551979+Varenas21@users.noreply.github.com>
2024-12-02 10:41:35 -0700

    Skills are now being added dynamically

--- MainScripts.js
+++ MainScripts.js
@@ -10,3 +10,48 @@ document.querySelector('nav').addEventListener('click',function(event) {
         event.preventDefault();
     }
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Skills data: categories and skills
+    const skillsData = [
+        {
+        category: "Programming Languages",
+        skills: ["C#", "Java", "HTML5/CSS", "C++", "JavaScript", "React.js", "Node.js"]
+    },
+    {
+        category: "Tool & Technologies",
+        skills: ["Visual Studio", "Git | Github | BitBucket"]
+    },
+    {
+        category: "Other Skills",
+        skills: ["Team and Project Management", "Agile Development", "UI/UX Design", "Game Development", "Game AI", "Game Design", "Effective Communication"]
+    }
+    ];
+
+    // Use the contaienr to append the items
+    const skillsContainer = document.querySelector('.skills-container');
+
+    // Loop through each item and generate it dynamically
+    skillsData.forEach(skillCategory => {
+        const categoryDiv = document.createElement('div');
+        categoryDiv.classList.add('skills-list');
+
+        // Create category title
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.textContent = skillCategory.category;
+        categoryDiv.appendChild(categoryTitle);
+
+        // Create list of skills
+        const skillsList = document.createElement('ul');
+        skillCategory.skills.forEach(skill => {
+            const listItem = document.createElement('li');
+            listItem.textContent = skill;
+            skillsList.appendChild(listItem);
+        });
+
+        // Append items to container
+        categoryDiv.appendChild(skillsList);
+        skillsContainer.appendChild(categoryDiv);
+    })
+});
+
